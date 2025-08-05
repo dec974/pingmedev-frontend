6bdb1a28cd9839a7b2659d75b8f682f5643d4d0e
Sébastien <sepi.mailpro@gmail.com>
2025-08-05 11:54:13 +0200

    UI kit mod

--- ui-kit/organisms/Header.js
+++ ui-kit/organisms/Header.js
@@ -4,15 +4,15 @@ import styles from "./Header.module.css";
 import { FaEnvelope } from "react-icons/fa";
 import Image from "next/image";
 
-export default function Header({ unreadCount = 0 }) {
+export default function Header({ unreadCount = 0, onLoginClick }) {
   return (
     <header className={styles.header}>
       <div className={styles.navbar}>
-        
         <Link href="/" className={styles.logo}>
-        <Image src="/logo.png" width={100} height={65} className={styles.logoImg}/>
+          <Image src="/logo.png" width={100} height={65} className={styles.logoImg}/>
           PingMe.dev
         </Link>
+
         <nav className={styles.navLinks}>
           <Link href="/messages" className={styles.iconLink}>
             <FaEnvelope className={styles.icon} />
@@ -20,9 +20,13 @@ export default function Header({ unreadCount = 0 }) {
               <span className={styles.badge}>{unreadCount}</span>
             )}
           </Link>
-          <Button variant="primary">Connexion</Button>
+
+          <Button variant="primary" onClick={onLoginClick}>
+            Connexion
+          </Button>
         </nav>
       </div>
     </header>
   );
 }
+
