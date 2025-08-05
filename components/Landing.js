de5ff00dc0b50fa42e3d358e21fc8aa9ee57c514
Adrien <adrien.bordeaux33@outlook.com>
2025-08-05 11:56:06 +0200

    maj landing 12H

--- components/Landing.js
+++ components/Landing.js
@@ -10,19 +10,53 @@ export default function Home() {
       <div
         style={{
           position: "absolute",
-          bottom: "20px",
-          right: "20px",
+          left: "300px",
+          top: "50%",
         }}
       >
+        <h1
+          style={{
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: "#333",
+            marginBottom: "20px",
+            maxWidth: "400px",
+            lineHeight: "1.4",
+          }}
+        >
+          La plateforme des développeurs ouverte à tous les parcours.
+        </h1>
+        <button
+          style={{
+            padding: "15px 30px",
+          }}
+        >
+          Visiter
+        </button>
+        <text
+          style={{
+            fontSize: "24px",
+            color: "#333",
+            marginBottom: "20px",
+            maxWidth: "400px",
+            lineHeight: "1.4",
+          }}
+        >
+          *Vous devrez vous connecter pour échanger avec les autres
+          utilisateurs.
+        </text>
+      </div>
+
+      <div>
         <img
           src="/landingImage.png"
           alt="Landing Image"
           style={{
-            maxWidth:
-              window.innerWidth < 768
-                ? //768px est la limite entre phone/tablette et ecran pc
-                  "600px"
-                : "1000px",
+            position: "absolute",
+            bottom: "20px",
+            right: "50px",
+            objectFit: "contain",
+            width: "50%",
             height: "auto",
           }}
         />
