d2bcb0784ad2f2f4974360f1ad228d42289c231a
Carron cedric <carron.cedric@laposte.net>
2025-08-10 02:09:23 +0200

    posts edit

--- modules/formatDate.js
+++ modules/formatDate.js
@@ -15,9 +15,13 @@ function formatDate(createdAt) {
     } else if (duration.asHours() >= 1) { 
         // Si + d'une heure, afficher en heures
         timeAgo = `il y a ${Math.floor(duration.asHours())}h`;
-    } else { 
+    } else if (duration.asMinutes() > 1) { 
         // Sinon afficher les minutes
         timeAgo = `il y a ${Math.floor(duration.asMinutes())}min`;
+    }
+    else {
+        // Sinon afficher les secondes
+        timeAgo = `à l'instant`;
     }
     return timeAgo;
 };
