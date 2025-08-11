e9b5a8e63e7858e6f7efec354c12bc1e8c74fd02
Sébastien <sepi.mailpro@gmail.com>
2025-08-11 11:56:43 +0200

    mod MainLayout

--- ui-kit/template/MainLayout.js
+++ ui-kit/template/MainLayout.js
@@ -1,6 +1,6 @@
 import Header from "../organisms/Header";
 import Footer from "../organisms/Footer";
-import styles from "../template/MainLayout.module.css";
+
 
 export default function MainLayout({ children, className }) {
   return (
