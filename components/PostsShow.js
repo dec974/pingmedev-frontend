7a6cf6e389304809499dbb1c6e7db9cf8a8eaec1
Laurem <karinelauremartins@gmail.com>
2025-08-13 17:58:28 +0200

    handle fetch on popover

--- components/PostsShow.js
+++ components/PostsShow.js
@@ -12,6 +12,7 @@ import Icon from "../ui-kit/atoms/Icon";
 import Modal from "react-modal";
 import { FaPencil } from "react-icons/fa6";
 import { MdGroupAdd, MdPersonRemoveAlt1 } from "react-icons/md";
+import ProfilePopover from "./ProfilePopover";
 
 function PostsShow() {
   const router = useRouter();
@@ -177,7 +178,7 @@ function PostsShow() {
   }
   // affichage de icons langages
   const languagesList = post.languages.map((language, index) => {
-    return <Icon key={index} language={language} size={60} />;
+    return <Icon key={index} language={language} size={50} />;
   });
 
   console.log("Post data:", post);
@@ -205,16 +206,24 @@ function PostsShow() {
         </div>
         <div className={styles.postContainer}>
           <div className={styles.postHeader}>
-            <div className={styles.postAuthor}>
-              <Image
-                src="/avatar.png"
-                width={60}
-                height={60}
-                className={styles.logoImg}
-                alt="PingMe logo"
-              />
-              {post ? post.userId.username : "Loading..."}
-            </div>
+            <ProfilePopover
+              userIdOrUsername={post?.userId?.username}
+              trigger={
+                <div className={styles.postAuthor}>
+                  <Image
+                    src="/avatar.png"
+                    width={60}
+                    height={60}
+                    className={styles.logoImg}
+                    alt="PingMe logo"
+                  />
+                  {post ? post.userId.username : "Loading..."}
+                </div>
+              }
+              onDiscuss={handleNewMessage}
+              onToggleFollow={toggleFollowAuthor}
+              followedAuthor={followedAuthor}
+            />
 
             <div className={styles.postHeaderTitle}>
               <h1 className={styles.postTitle}>{post.title}</h1>
@@ -232,7 +241,7 @@ function PostsShow() {
                 >
                   {followedAuthor ? (
                     <MdPersonRemoveAlt1
-                      size={32}
+                      size={38}
                       color="var(--secondary-color)"
                     />
                   ) : (
