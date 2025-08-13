87c9f30c66fd6e56f2620d8861810e0b2dd6549f
Adrien <adrien.bordeaux33@outlook.com>
2025-08-13 14:12:12 +0200

    e

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,9 @@ import GitHubProvider from "next-auth/providers/github";
 export default NextAuth({
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
